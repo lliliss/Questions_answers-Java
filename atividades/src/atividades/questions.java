3f31c82768ad1dfac117fc21fada37062ff2adbc
Alice Ferreira <alicefc.arco@gmail.com>
2024-09-23 18:11:39 -0300

    First quest.

---
+++ atividades/src/atividades/questions.java
@@ -0,0 +1,21 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class questions {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um número: ");
+		int num = sc.nextInt();
+		
+		if (num >= 0) {
+			System.out.println("O número digitado é positivo!");
+		} else {
+			System.out.print("O número digitado é negativo!");
+		}
+		sc.close();
+	}
+
+}
